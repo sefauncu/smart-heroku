9956078d6a5b6ad045d9e87f582d18f8340f68b9
sefauncuetiya <sefa.uncu@etiya.com>
2019-12-28 17:28:42 +0300

    hello

--- src/main/java/com/heroku/herokube/HomeController.java
+++ src/main/java/com/heroku/herokube/HomeController.java
@@ -1,5 +1,6 @@
 package com.heroku.herokube;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/home")
+    @CrossOrigin(origins = "http://localhost:4200")
     public String hi(){
         return "Hello Smart Heroku";
     }
