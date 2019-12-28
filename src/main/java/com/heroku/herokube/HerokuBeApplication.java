1bc7a13479af42baf34124a802f104ff098653fd
sefauncuetiya <sefa.uncu@etiya.com>
2019-12-28 17:01:22 +0300

    fix

---
+++ src/main/java/com/heroku/herokube/HerokuBeApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.herokube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heroku/herokube/HerokuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.herokube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
