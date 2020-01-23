8d6e569c8f1f9ec9529d36503c28b0b0f58b368d
sefauncuetiya <sefa.uncu@etiya.com>
2020-01-23 16:38:02 +0300

    fix

--- src/main/java/com/heroku/herokube/HomeController.java
+++ src/main/java/com/heroku/herokube/HomeController.java
@@ -1,14 +1,27 @@
 package com.heroku.herokube;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HomeController {
 
+//    @Autowired
+//    private HomeService homeService;
+
     @GetMapping("/home")
     public Message hi() {
-        Message message = new Message("by Emrah & Emrullah & Sefa & Süleyman");
+        Message message = new Message("hi heroku");
+//        homeService.save(message);
         return message;
     }
+
+//    @GetMapping("/list")
+//    public List<Message> findAll() {
+//        List<Message> messages = homeService.findAll();
+//        return messages;
+//    }
 }
